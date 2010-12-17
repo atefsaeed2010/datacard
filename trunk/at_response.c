a8d6e6140e2308e6b4c0b2d9b14ccfeb072d10d6
bg_one@mail.ru <bg_one@mail.ru@1418af2e-0bf0-1a57-cff8-d4ec9bae2ddc>
2010-12-17 11:14:43 +0000

    ccwa set/status only when offline -> online

--- trunk/at_response.c
+++ trunk/at_response.c
@@ -1437,9 +1437,10 @@ static int at_response_creg (struct pvt* pvt, char* str, size_t len)
 
 	if (d)
 	{
+		/* only if gsm_registered 0 -> 1 ? */
+		if(!pvt->gsm_registered)
+			at_enque_set_ccwa(&pvt->sys_chan, 0, 0, CONF_SHARED(pvt, call_waiting));
 		pvt->gsm_registered = 1;
-		/* FIXME: only if gsm_registered 0 -> 1 ? */
-		at_enque_set_ccwa(&pvt->sys_chan, 0, 0, CONF_SHARED(pvt, call_waiting));
 	}
 	else
 	{
